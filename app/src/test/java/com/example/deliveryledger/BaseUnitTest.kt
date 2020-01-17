1cd3153d8e8f0cd5a18adb06f04a0dbab300e16f
Ishan Gaurav <ishangaurav@gmail.com>
2020-01-17 16:37:59 +0530

    Implemented review suggestion

--- app/src/test/java/com/example/deliveryledger/BaseUnitTest.kt
+++ app/src/test/java/com/example/deliveryledger/BaseUnitTest.kt
@@ -1,15 +1,18 @@
 package com.example.deliveryledger
 
+import android.os.Build
+import com.example.deliveryledger.di.DeliveryApplication
 import org.junit.Before
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 
-@RunWith(JUnit4::class)
-/*@RunWith(
+//@RunWith(JUnit4::class)
+@RunWith(
     RobolectricTestRunner::class)
-    @Config(application=DeliveryApplication::class, sdk = [Build.VERSION_CODES.P])*/
+    @Config(application= DeliveryApplication::class, sdk = [Build.VERSION_CODES.P])
 abstract class BaseUnitTest{
 
     @Before
